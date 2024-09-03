eafd80a808a72cc7c425cd8a5a375ce06a5d3017
jett2 <jettmorrow@vt.edu>
2024-09-03 12:35:05 -0400

    more work on CP

--- F24P1SongsDB_HashTable_AdjList_Starter/src/CommandProcessor.java
+++ F24P1SongsDB_HashTable_AdjList_Starter/src/CommandProcessor.java
@@ -36,6 +36,49 @@ public class CommandProcessor implements Interpreter {
         //we first have to process the first word (insert, remove, print)
         
         //depending on first word they have dif following input
+        String command = oneLine.next();
+        
+        switch (command) {
+            case "insert": 
+                // insert {artist-name}<SEP>{song-name}
+                oneLine.useDelimiter("<SEP>");
+                String artist = oneLine.next();
+                String song = oneLine.next();
+                //Controller.insert(String artist, String song);
+                break;
+            case "remove":
+                // remove {artist|song}{name}
+                String artistOrName = oneLine.next();
+                //captures the remainder of the line
+                String name = oneLine.findInLine(".*");
+                switch (artistOrName) {
+                    case "artist":
+                        //controller.removeArtist(name);
+                        break;
+                    case "song":
+                        //controller.removeSong(name);
+                        break;
+                }
+                break;
+                
+            case "print":
+                // print {artist|song|graph}
+                switch (oneLine.next()) {
+                    case "artist":
+                        //controller.printArtist(PrintWriter output);
+                        break;
+                    case "song":
+                        //controller.printSong(PrintWriter output);
+                        break;
+                    case "graph":
+                        //controller.printGraph(PrintWriter output);
+                        break;
+                }
+                break;
+            default:
+                //invalid command??
+                break;
+        }
     }
 
 }

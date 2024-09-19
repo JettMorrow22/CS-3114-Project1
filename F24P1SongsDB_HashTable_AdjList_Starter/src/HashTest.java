125aa41385c630c3adfed965e5e250fe29166744
jett2 <jettmorrow@vt.edu>
2024-09-19 11:19:51 -0400

    changed hash to resize if an insert would be >= 50%

--- F24P1SongsDB_HashTable_AdjList_Starter/src/HashTest.java
+++ F24P1SongsDB_HashTable_AdjList_Starter/src/HashTest.java
@@ -168,12 +168,12 @@ public class HashTest extends TestCase {
                                                                // 1, prob 4,
                                                                // prob 9
         // add 9
-
-        assertFalse(hashTable.checkAndResize());
+        //if we added another it would be >= 50% so resize?
+        assertTrue(hashTable.checkAndResize());
         assertTrue(hashTable.getRecords()[0].getKey().equals("Jett Morrow"));
         assertTrue(hashTable.getRecords()[1].getKey().equals("Adam"));
-        assertTrue(hashTable.getRecords()[4].getKey().equals("Adamasa"));
-        assertTrue(hashTable.getRecords()[9].getKey().equals(
+        assertTrue(hashTable.getRecords()[10].getKey().equals("Adamasa"));
+        assertTrue(hashTable.getRecords()[4].getKey().equals(
             "JENNA KANTOWSKI"));
 
         // check resizing with tombstones
